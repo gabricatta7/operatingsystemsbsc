1e60aaad842b0e8cc1f99612a2dd5e9340cabfd7
Nicola <nicola.bicocchi@gmail.com>
2022-03-14 04:33:31 -0700

    update

--- bash/code/lab02/es21.sh
+++ bash/code/lab02/es21.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+USAGE="usage: $0 filename permissions"
+
+if [ $# -ne 2 ]; then
+	echo "$USAGE"
+	exit 1
+fi
+
+if [ -f "$1" ]; then
+	echo "$USAGE"
+	exit 1
+fi
+
 touch "$1"
 chmod "$2" "$1"
 
