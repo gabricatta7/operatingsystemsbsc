3aacb699171885bbece68875801d8d3ac7ea6696
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2022-04-28 16:11:15 +0200

    Update

--- c/code/lab03/0310.c
+++ c/code/lab03/0310.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
 
-#define ROWS 10
-#define COLS 10
+#define ROWS 3
+#define COLS 3
 
-void show_addresses(int rows, int cols, int v[][cols]) {
-    int i, j;
+void fill_matrix(unsigned rows, unsigned cols, int m[][cols]) {
+    unsigned i, j, count = 1;
 
     for (i = 0; i < rows; i++) {
         for (j = 0; j < cols; j++) {
-            printf("%p -> %d\n", &v[i][j], v[i][j]);
+            m[i][j] = (int)count++;
+        }
+    }
+}
+
+void show_addresses(unsigned rows, unsigned cols, int m[][cols]) {
+    unsigned i, j;
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%p -> %d\n", &m[i][j], m[i][j]);
         }
         printf("\n");
     }
 }
 
-#define ROWS 10
-#define COLS 10
+/*
+ * If this function correctly prints the matrix, its elements have to be contiguous in memory
+ */
+void show_matrix(unsigned rows, unsigned cols, int *m) {
+    unsigned i, j;
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%4d", m[i * cols + j]);
+        }
+        printf("\n");
+    }
+}
 
 int main(void) {
     int v[ROWS][COLS];
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            v[i][j] = (i + 1) * (j + 1);
-        }
-    }
+
+    fill_matrix(ROWS, COLS, v);
+    show_matrix(ROWS, COLS, (int *)v);
     show_addresses(ROWS, COLS, v);
 }
 
