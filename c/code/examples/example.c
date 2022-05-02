ba79f8b7c373762a6e70d7dedd7fe2ee30a03cea
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2022-05-02 20:22:48 +0200

    Update

--- c/code/examples/example.c
+++ c/code/examples/example.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main(void) {
-    char *str = "hello world!";
-    char *str_copy = strdup(str);
 
-    printf("%s %s\n", str, str_copy);
-
-    free(str_copy);
-
-    /* error: pointer being freed was not allocated */
-    free(str);
 }
