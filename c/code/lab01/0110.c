ae337bed00bff1ec7957cdc8f26792a560934ac1
Nicola Bicocchi <nbicocchi@unimore.it>
2023-05-04 22:41:58 +0200

    update

--- c/code/lab01/0110.c
+++ c/code/lab01/0110.c
@@ -7,7 +7,7 @@ int is_prime(unsigned v) {
     if (v == 0) return FALSE;
     if (v == 1) return TRUE;
 
-    for (unsigned  i = 2; i < v; i++) {
+    for (unsigned i = 2; i < v; i++) {
         if (v % i == 0) {
             return FALSE;
         }
